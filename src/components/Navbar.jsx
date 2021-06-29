357ded0ff957fa412f1ece13d049b5407c6df81e
yannis94 <bengueci.yannis@gmail.com>
2021-06-29 10:57:13 +0200

    submenu in place and stable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,34 @@
 import { Link, withRouter } from "react-router-dom";
+import { useState } from 'react'
 import '../styles/navbar.css'
 
 function Navbar() {
     
+    const [isOpen, setIsOpen] = useState({
+        'reseau': false,
+        'pacte': false
+    })
+
+    function handleMouseEnter(e) {
+        while(e.target.tagName !== 'LI') {
+            console.log('oops')
+            e.target = e.target.parentElement
+        }
+        let myObj = {... isOpen}
+        myObj[e.target.id] = true
+        setIsOpen(myObj)
+    }
+
+    function handleMouseLeave(e) {
+        while (e.target.tagName !== 'LI') {
+            console.log('oops')
+            e.target = e.target.parentElement
+        }
+        let myObj = { ...isOpen }
+        myObj[e.target.id] = false
+        setIsOpen(myObj)
+    }
+
     return (
         <div className="tgtg-navbar">
             <Link to="/">
@@ -29,6 +55,43 @@ function Navbar() {
                         <Link className={window.location.pathname === "/blog" ? `tgtg-navbar-link link-active` : "tgtg-navbar-link"} to="/blog">
                             Blog
                         </Link>
+                    </li>
+                    <li id="reseau" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                        <Link className={window.location.pathname === "/reseau" ? `tgtg-navbar-link link-active` : "tgtg-navbar-link"} to="/">
+                            Réseau anti-gaspi
+                        </Link>
+                        {isOpen.reseau ? 
+                            <ul className="tgtg-navbar-sub">
+                                <Link className="tgtg-navbar-sublink" to="/">
+                                    Les villes partenaires
+                                </Link>
+                                <Link className="tgtg-navbar-sublink" to="/">
+                                    Les engagements
+                                </Link>
+                            </ul>
+                            :
+                            <div className="tgtg-sub-triangle"></div>
+                        }
+                    </li>
+                    <li id="pacte" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                        <Link className={window.location.pathname === "/pacte" ? `tgtg-navbar-link link-active` : "tgtg-navbar-link"} to="/">
+                            Le pacte
+                        </Link>
+                        {isOpen.pacte ?
+                            <ul className="tgtg-navbar-sub">
+                                <Link className="tgtg-navbar-sublink" to="/">
+                                    Les engagements
+                                </Link>
+                                <Link className="tgtg-navbar-sublink" to="/">
+                                    Les signataires
+                                </Link>
+                                <Link className="tgtg-navbar-sublink" to="/">
+                                    Signer le pacte
+                                </Link>
+                            </ul>
+                            :
+                            <div className="tgtg-sub-triangle"></div>
+                        }
                     </li>
                     <li>
                         <Link className={window.location.pathname === "/exposition" ? `tgtg-navbar-link link-orange link-active` : "tgtg-navbar-link link-orange"} to="/exposition">
